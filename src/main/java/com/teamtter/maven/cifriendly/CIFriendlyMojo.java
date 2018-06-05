260be3b244cba5e9548b7b424d38c12d4198fede
francois.marot <francois.marot@olea-medical.com>
2018-06-05 15:55:10 +0200

    minor refactor

--- src/main/java/com/teamtter/maven/cifriendly/CIFriendlyMojo.java
+++ src/main/java/com/teamtter/maven/cifriendly/CIFriendlyMojo.java
@@ -2,11 +2,8 @@ package com.teamtter.maven.cifriendly;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -30,37 +27,44 @@ import lombok.extern.slf4j.Slf4j;;
 		defaultPhase = LifecyclePhase.NONE,	//
 		requiresProject = true,				//
 		threadSafe = false,					//
-		aggregator = true,
+		aggregator = true,					// only execute on the parent pom, root of the maven tree (not on children)
 		requiresDependencyCollection = ResolutionScope.NONE,	//
 		requiresDependencyResolution = ResolutionScope.NONE)
 @Slf4j
 public class CIFriendlyMojo extends AbstractMojo {
 
-	private List<String> excludedDirs = Arrays.asList(".git", "target", "src", ".idea", ".settings");
+	private List<String>	excludedDirs	= Arrays.asList(".git", "target", "src", ".idea", ".settings");
 
 	@Parameter(defaultValue = "${project}", readonly = true)
 	@Setter
 	private MavenProject	mavenProject;
-	
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		
+
 		final String version = mavenProject.getVersion();
 		log.info("Maven project version read: {}", version);
+		
+		String pattern = "<version>${revision}</version>";
 		
 		Consumer<Path> pomFileConsumer = path -> {
 			try (Stream<String> lines = Files.lines(path)) {
 				List<String> replaced = lines
-						.map(line -> line.replace("<version>${revision}</version>", "<version>" + version + "</version>"))
+						.peek(line -> {
+							if (line.contains(pattern)) {
+								log.info("Will replace pattern {} in {}", pattern, path);
+							}	
+						})
+						.map(line -> line.replace(pattern, "<version>" + version + "</version>"))
 						.collect(Collectors.toList());
 				Files.write(path, replaced);
 			} catch (IOException e) {
 				throw new RuntimeException("Unable to modify file " + path);
 			}
 		};
-		
-		Predicate<Path> isPomFile = path -> path.getFileName().toString().equals("pom.xml"); 
-		
+
+		Predicate<Path> isPomFile = path -> path.getFileName().toString().equals("pom.xml");
+
 		ChangePomFileVisitor pomVisitor = new ChangePomFileVisitor(excludedDirs, isPomFile, pomFileConsumer);
 		try {
 			Files.walkFileTree(new File(".").toPath(), pomVisitor);
@@ -69,49 +73,4 @@ public class CIFriendlyMojo extends AbstractMojo {
 		}
 	}
 
-	class ChangePomFileVisitor extends SimpleFileVisitor<Path> {
-
-		private List<String>	excludedDirs;
-		private Predicate<Path>	fileMatchesPredicate;
-		private Consumer<Path>	matchingFilesConsumer;
-
-		public ChangePomFileVisitor(List<String> excludedDirs, Predicate<Path> fileMatchesPredicate, Consumer<Path> matchingFilesConsumer) {
-			this.excludedDirs = excludedDirs;
-			this.fileMatchesPredicate = fileMatchesPredicate;
-			this.matchingFilesConsumer = matchingFilesConsumer;
-		}
-
-		@Override
-		public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-			String fileName = dir.getFileName().toString();
-			boolean excluded = excludedDirs.contains(fileName);
-			if (excluded) {
-				log.debug("Skipping dir " + dir);
-				return FileVisitResult.SKIP_SUBTREE;
-			} else {
-				return FileVisitResult.CONTINUE;
-			}
-		}
-
-		@Override
-		public FileVisitResult visitFile(Path file, BasicFileAttributes attr) throws IOException {
-			log.debug("Visiting {}", file);
-			if (attr.isSymbolicLink()) {
-				return FileVisitResult.CONTINUE;
-			} else if (attr.isRegularFile()) {
-				if (fileMatchesPredicate.test(file)) {
-					matchingFilesConsumer.accept(file);
-				}
-				return FileVisitResult.CONTINUE;
-			} else {
-				return FileVisitResult.CONTINUE;
-			}
-		}
-
-		@Override
-		public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
-			log.trace("Directory done: {}", dir);
-			return FileVisitResult.CONTINUE;
-		}
-	}
 }
---
+++ src/main/java/com/teamtter/maven/cifriendly/ChangePomFileVisitor.java
@@ -0,0 +1,59 @@
+package com.teamtter.maven.cifriendly;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import lombok.extern.slf4j.Slf4j;;
+
+@Slf4j
+class ChangePomFileVisitor extends SimpleFileVisitor<Path> {
+
+	private List<String>	excludedDirs;
+	private Predicate<Path>	fileMatchesPredicate;
+	private Consumer<Path>	matchingFilesConsumer;
+
+	public ChangePomFileVisitor(List<String> excludedDirs, Predicate<Path> fileMatchesPredicate, Consumer<Path> matchingFilesConsumer) {
+		this.excludedDirs = excludedDirs;
+		this.fileMatchesPredicate = fileMatchesPredicate;
+		this.matchingFilesConsumer = matchingFilesConsumer;
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+		String fileName = dir.getFileName().toString();
+		boolean excluded = excludedDirs.contains(fileName);
+		if (excluded) {
+			log.debug("Skipping dir " + dir);
+			return FileVisitResult.SKIP_SUBTREE;
+		} else {
+			return FileVisitResult.CONTINUE;
+		}
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attr) throws IOException {
+		log.debug("Visiting {}", file);
+		if (attr.isSymbolicLink()) {
+			return FileVisitResult.CONTINUE;
+		} else if (attr.isRegularFile()) {
+			if (fileMatchesPredicate.test(file)) {
+				matchingFilesConsumer.accept(file);
+			}
+			return FileVisitResult.CONTINUE;
+		} else {
+			return FileVisitResult.CONTINUE;
+		}
+	}
+
+	@Override
+	public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+		log.trace("Directory done: {}", dir);
+		return FileVisitResult.CONTINUE;
+	}
+}
