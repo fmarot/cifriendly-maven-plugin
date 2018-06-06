303b0dcbcecefd762c12204dd988ff9925173981
francois.marot <francois.marot@olea-medical.com>
2018-06-06 10:50:02 +0200

    added (simple) unit test

--- src/main/java/com/teamtter/maven/cifriendly/UnFlatten.java
+++ src/main/java/com/teamtter/maven/cifriendly/UnFlatten.java
@@ -17,16 +17,14 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
 
 import de.pdark.decentxml.Document;
 import de.pdark.decentxml.Element;
 import de.pdark.decentxml.XMLParser;
 import de.pdark.decentxml.XMLStringSource;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;;
 
-@Mojo(name = "unflatten",						// the goal
+@Mojo(name = "unflatten",					// the goal
 		defaultPhase = LifecyclePhase.NONE,	//
 		requiresProject = true,				//
 		threadSafe = false,					//
@@ -36,72 +34,89 @@ import lombok.extern.slf4j.Slf4j;;
 @Slf4j
 public class UnFlatten extends AbstractMojo {
 
-	private List<String>	excludedDirs	= Arrays.asList(".git", "target", "src", ".idea", ".settings");
+	private static final String	REVISION_VARIABLE	= "${revision}";
 
-	@Parameter(defaultValue = "${project}", readonly = true)
-	@Setter
-	private MavenProject	mavenProject;
+	private static final String	POM_XML				= "pom.xml";
+
+	private static List<String>	excludedDirs		= Arrays.asList(".git", "target", "src", ".idea", ".settings");
+
+	@Parameter(property = "rootDir", defaultValue = "${session.executionRootDirectory}")
+	private File				rootDir;
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		
+		Path rootPath = rootDir.toPath();
+		unflatten(rootPath);
+	}
+
+	protected static void unflatten(Path rootPath) throws MojoFailureException {
+		Path rootPom = Paths.get(rootPath.toString(), POM_XML);
+
 		String parentPomVersion;
 		try {
-			parentPomVersion = readVersionInParentPom();			
+			parentPomVersion = readVersionInParentPom(rootPath);
 		} catch (IOException e) {
 			throw new MojoFailureException("", e);
 		}
-			
+
 		PathConsumer pomFileConsumer = path -> {
+			// Prepare XML parser & model
 			XMLParser parser = new XMLParser();
-			final String visitorContent = new String (Files.readAllBytes(path), StandardCharsets.UTF_8);
-			final Document doc = parser.parse (new XMLStringSource (visitorContent));
-			
+			final String visitorContent = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+			final Document doc = parser.parse(new XMLStringSource(visitorContent));
+
+			// update the DOM
 			replaceVersionwithRevision(doc, parentPomVersion, path);
-			
-			specificUpdateForRootPom(parentPomVersion, path, doc);
-			
+			specificUpdateForRootPom(doc, parentPomVersion, path, rootPom);
+
+			// write back the result
 			Files.write(path, doc.toString().getBytes());
 		};
-		
-		Predicate<Path> isPomFile = path -> path.getFileName().toString().equals("pom.xml");
+
+		Predicate<Path> isPomFile = path -> path.getFileName().toString().equals(POM_XML);
 		ChangePomFileVisitor pomVisitor = new ChangePomFileVisitor(excludedDirs, isPomFile, pomFileConsumer);
-			
+
 		try {
-			Files.walkFileTree(new File(".").toPath(), pomVisitor);
+			Files.walkFileTree(rootPath, pomVisitor);
 		} catch (IOException e) {
 			throw new MojoFailureException("", e);
 		}
 	}
 
-	private void specificUpdateForRootPom(String parentPomVersion, Path path, final Document doc) throws IOException {
-		// add (or change the value) of the properties/revision node in parent pom with 'parentPomVersion'
-		if (Files.isSameFile(path, Paths.get("pom.xml"))) {
-			log.info("parent pom found");
-			Element revisionPropertyNode = doc.getChild("/project/properties/revision");
-			// TODO: handle the case where no revision node exist and create it
-			revisionPropertyNode.setText(parentPomVersion);
+	/** add (or change the value) of the properties/revision node in parent pom with 'parentPomVersion' */
+	private static void specificUpdateForRootPom(Document doc, String parentPomVersion, Path path, Path rootPom) throws IOException {
+		if (Files.isSameFile(path, rootPom)) {
+			log.info("parent pom found: {}", path);
+			if (parentPomVersion.equals(REVISION_VARIABLE)) {
+				log.info("Parent pom already unflattened => will not update it's node /project/properties/revision");
+			} else {
+				Element revisionPropertyNode = doc.getChild("/project/properties/revision");
+				if (revisionPropertyNode == null) {
+					throw new IOException("this pom has no properties/revision section so it was never CI friendly => can not unflatten...");
+				}
+				revisionPropertyNode.setText(parentPomVersion);
+			}
 		}
 	}
-	
-	private void replaceVersionwithRevision(Document doc, String parentPomVersion, Path path) throws IOException {
+
+	private static void replaceVersionwithRevision(Document doc, String parentPomVersion, Path path) throws IOException {
 		Element visitorVersionNode = doc.getChild("/project/version");
 		if (visitorVersionNode == null) {
 			visitorVersionNode = doc.getChild("/project/parent/version");
 		}
 		String text = visitorVersionNode.getText();
-		if (! text.equals(parentPomVersion)) {
+		if (!text.equals(parentPomVersion)) {
 			log.error("Found version {} in {} instead of version {}", text, path, parentPomVersion);
 			throw new IOException("Unexpected version in pom " + path);
 		}
-		visitorVersionNode.setText("${revision}");
+		visitorVersionNode.setText(REVISION_VARIABLE);
 		log.info("Rewriting pom {}", path);
 	}
 
-	private String readVersionInParentPom() throws IOException {
+	private static String readVersionInParentPom(Path rootDir) throws IOException {
 		XMLParser parser = new XMLParser();
-		final String content = new String (Files.readAllBytes(Paths.get("pom.xml")), StandardCharsets.UTF_8);
-        final Document doc = parser.parse (new XMLStringSource (content));
+		final String content = new String(Files.readAllBytes(Paths.get(rootDir.toString(), POM_XML)), StandardCharsets.UTF_8);
+		final Document doc = parser.parse(new XMLStringSource(content));
 		final Element versionNode = doc.getChild("/project/version");
 		final String version = versionNode.getText();
 		log.info("version used to unflatten: {}", version);
@@ -109,15 +124,15 @@ public class UnFlatten extends AbstractMojo {
 	}
 
 	public static void main(String[] args) throws IOException {
-        XMLParser parser = new XMLParser();
-        String content = new String (Files.readAllBytes(Paths.get("../test-fake-module/pom.xml")), StandardCharsets.UTF_8);
-		Document doc = parser.parse (new XMLStringSource (content));
-		
+		XMLParser parser = new XMLParser();
+		String content = new String(Files.readAllBytes(Paths.get("../test-fake-module/pom.xml")), StandardCharsets.UTF_8);
+		Document doc = parser.parse(new XMLStringSource(content));
+
 		List<Element> children = doc.getRootElement().getChildren();
 		Element child = doc.getChild("/project/properties/revision");
-		
+
 		children.forEach(child2 -> System.out.println(child2.getChildPath()));
-		
+
 		System.out.println(child.getText().toString());
 	}
 
---
+++ src/test/java/com/teamtter/maven/cifriendly/UnFlattenTest.java
@@ -0,0 +1,42 @@
+package com.teamtter.maven.cifriendly;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileSystemUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnFlattenTest {
+	
+	Path resources = Paths.get("target/test-classes/unflatten");
+	
+	@Test
+	public void testSinglePom() throws MojoFailureException, IOException {
+		Path relativeTestPath = Paths.get("singlePom");
+		Path relativeReferenceTestPath = Paths.get("singlePom-reference");
+		Path testPath  = resources.resolve(relativeTestPath);
+		Path referenceTestPath  = resources.resolve(relativeReferenceTestPath);
+		UnFlatten.unflatten(testPath);
+		assertDirectoriesAreEquals(testPath, referenceTestPath);
+		// execute it a second time to make sure the results are always the same (the second time /properties/revision/X.Y.Z-SNAPSHOT must not be replaced by ${revision} !)
+		UnFlatten.unflatten(testPath);
+		assertDirectoriesAreEquals(testPath, referenceTestPath);		
+	}
+	
+	private void assertDirectoriesAreEquals(Path testPath, Path referenceTestPath) throws IOException {
+		assertTrue("The files differ!", FileUtils.contentEquals(
+				testPath.resolve("pom.xml").toFile(),
+				referenceTestPath.resolve("pom.xml").toFile()));
+	}
+
+	@Test
+	public void testMultiModuleMavenProject() {
+		
+	}
+}
